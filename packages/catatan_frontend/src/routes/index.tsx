8dc755b07ad5ed5e0e96318daa002856ac087e17
Haekal Kalipaksi <hkalipaksi@proton.me>
2025-11-02 20:36:18 +0700

    frontend Implement styling
    * backend: implement create new notes
    
    * backend: Impelemnt reading from .env file while in dev environment
    
    * frontend: Implement editor
    
    * frontend: Implement styling and command

--- packages/catatan_frontend/src/routes/index.tsx
+++ packages/catatan_frontend/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/solid-router";
 import { Editor } from "~/components/editor";
+import { Header } from "~/components/layout/header";
 
 export const Route = createFileRoute("/")({
   component: Login,
@@ -7,8 +8,11 @@ export const Route = createFileRoute("/")({
 
 function Login() {
   return (
-    <div class="relative w-full text-base">
-      <Editor />
+    <div class="flex flex-col relative w-full text-base ">
+      <Header />
+      <div class="relative w-full my-16">
+        <Editor />
+      </div>
     </div>
   );
 }
