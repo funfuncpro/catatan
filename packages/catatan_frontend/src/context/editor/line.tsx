5ee65f89359f49024705de4432167cfd150c7b9e
Haekal Kalipaksi <hkalipaksi@proton.me>
2025-11-25 15:45:18 +0700

    frontend: Remove unused code

--- packages/catatan_frontend/src/context/editor/line.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from "solid-js";
-import { Cursor } from "~/components/canvas-editor/operation/cursor";
-
-export function getCursorPosition(cursor: Cursor) {
-  return `${cursor.selection.anchor.path}:${cursor.selection.anchor.offset}`;
-}
